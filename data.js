92f8f92f277fb84047e5f921aa546ac682240e32
Anand Raj <49033626+anandcoderboi@users.noreply.github.com>
2022-11-06 17:31:26 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+import Airpod from "./images/AirPod.jpg"
+import Dslr from "./images/Dslr.jpg"
+import earphone from "./images/earphone.jpg"
+import microphone from "./images/microPhone.jpg"
+import Smartphone from "./images/SmartPhone.jpg"
+import SmartWatch from "./images/SmartWatch.jpg"
+import usbCable from "./images/usbCable.jpg"
+
+
+const list = [
+  { id: 1, name: "Dslr", price: 300, image: Dslr, amount: 1, },
+  { id: 2, name: "Airpod", price: 3000, image: Airpod, amount: 1, },
+  { id: 3, name: "earphone", price: 5500, image: earphone, amount: 1,},
+  { id: 4, name: "microPhone", price: 4500, image: microphone, amount: 1,},
+  { id: 5, name: "SmartWatch", price: 500, image: SmartWatch, amount: 1,},
+  { id: 6, name: "SmartPhone", price: 8900, image: Smartphone, amount: 1,},
+  { id: 7, name: "usbCable", price: 7000, image: usbCable, amount: 1,},
+];
+
+export default list;
